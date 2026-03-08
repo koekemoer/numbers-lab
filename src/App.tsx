dd7ba109a58ba71a2450f190bc9886837b13f05f
Lourens Koekemoer <lourens.koekemoer@entelect.co.za>
2026-03-08 17:47:24 +0200

    home page change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import PrimeGenerator from './pages/PrimeGenerator';
 
